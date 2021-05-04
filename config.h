163008851a841cb69af07ae5d4134475d252970c
Ryan Denoux <ryanpdenoux@gmail.com>
2021-05-04 19:22:13 +0200

    Rearrange for DOGE
    Signed-off-by: Ryan Denoux <ryanpdenoux@gmail.com>

--- config.h
+++ config.h
@@ -1,20 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"⌨", "sb-kbselect", 0, 30}, */
+	{"⌨", "sb-kbselect", 0, 30},
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"sb-tasks",	10,	26}, */
 	/* {"",	"sb-music",	0,	11}, */
-	/* {"",	"sb-pacpackages",	0,	8}, */
-	{"",	"sb-susepackages",	0,	8},
+	{"",	"sb-pacpackages",	0,	8},
+	/* {"",	"sb-susepackages",	0,	8}, */
 	/* {"",	"sb-news",		0,	6}, */
 	/* {"",	"sb-crypto",	0,	13}, */
 	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
 	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
 	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
-	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
+	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
+	{"",	"sb-price doge Doge 🐶",	9000,	23},
 	/* {"",	"sb-torrent",	20,	7}, */
 	/* {"",	"sb-memory",	10,	14}, */
 	/* {"",	"sb-moonphase",	18000,	17}, */
@@ -24,8 +25,8 @@ static const Block blocks[] = {
 	/* {"",	"sb-nettraf",	1,	16}, */
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
-	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
+	{"",	"sb-clock",	60,	1},
 	/* {"",	"sb-help-icon",	0,	15}, */
 };
 
